e397648f9dd200d228d457a026fd5ecc98b79803
Kellie Manasco <kellie.manasco@gmail.com>
2021-03-16 21:54:47 -0400

    Adds a grid in css to test click actions. Mostly working on getting the gameBoard module functioning in the console. I intend to tie the array index values to the key-data values in the html for updating the UI display

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 /// displayController module
-
+const displayController = function(move) {
+    document.querySelector(move).innerHTML = `${players.marker}`
+}
 
 /// gameBoard module
+let gameBoard = (function(){
+    let board = new Array(9);
+    let marker = 'x';
+    let divOne = document.querySelector(".one");
+    let divTwo = document.querySelector(".two");
 
+    divOne.addEventListener('click', () => {
+        makeMove(0, marker);
+    })
+    divTwo.addEventListener('click', () => {
+        makeMove(1, marker);
+    })
+    
+    
+    function makeMove(place, mark) {
+        if (board[place] === undefined) {
+            board[place] = mark;
+            console.log(board);
+            if (marker === 'x') marker = 'o';
+            else marker = 'x';
+        }
+    }
+    console.log(board);
+})();
 
 /// players factory function
+const Players = function(name, marker) {
+    this.name = name;
+    this.marker = marker;
+}
